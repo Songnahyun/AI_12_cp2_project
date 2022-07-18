a6ebc078b590b8f9f49afe37521f8a741ce628b8
asd3656 <59529622+asd3656@users.noreply.github.com>
2022-07-18 18:41:32 +0900

    PostgreSQL 연동

---
+++ cp2_db.py
@@ -0,0 +1,28 @@
+import psycopg2
+import pandas as pd
+from sqlalchemy import create_engine # sql에 csv파일 보내기
+
+#pip install psycopg2
+#pip install pandas
+#pip install sqlalchemy
+
+host = host="heffalump.db.elephantsql.com"
+database="kyndpfqh"
+user="kyndpfqh"
+password="oVto9ymfg0TXiBFe7Ryo6J1BCVInAwG4"
+
+conn = psycopg2.connect( # 커넥션 생성
+    host=host,
+    user=user,
+    password=password,
+    database=database
+)
+
+cur = conn.cursor() # 커서 생성
+
+wanted_file = pd.read_csv('b.csv')
+engine = create_engine('postgresql://kyndpfqh:oVto9ymfg0TXiBFe7Ryo6J1BCVInAwG4@heffalump.db.elephantsql.com/kyndpfqh')
+
+wanted_file.to_sql("b", engine)
+
+conn.commit()
